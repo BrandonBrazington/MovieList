f918894b3675c86702f7a4ded562eb995e952e14
Brandon Brazington <brandon@brazington.tech>
2019-11-08 19:31:33 -0800

    Randomly picking no longer picks the same movie until all movies have been picked

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,34 @@ import PropTypes from 'prop-types';
 import "./css/Header.css";
 
 class Header extends React.Component {
+    pickedMovies = [];
+
     pickMovie(movies, viewed, unviewed) {
         let filteredMovies = movies.filter((movie) => {
             if (viewed && movie.viewed) { return true }
             if (unviewed && !movie.viewed) { return true }
             return false;
         })
-        let currentPick = filteredMovies[Math.floor(Math.random() * filteredMovies.length)];
-        console.log(currentPick);
+        let currentPick = "";
+        while (currentPick === "") {
+            currentPick = filteredMovies[Math.floor(Math.random() * filteredMovies.length)];
+            if (this.pickedMovies.indexOf(currentPick) !== -1) currentPick = ""
+        }
+        this.pickedMovies.push(currentPick);
         window.open("https://www.imdb.com/title/" + currentPick.imdb, "_blank");
+        if (this.pickedMovies.length === filteredMovies.length) {
+            alert("All movies have been randomly picked now. The list has been reset.");
+            this.pickedMovies = [];
+        }
     }
 
     render() {
         return (
             <header className="navbar-fixed-top">
                 <h1>{this.props.listName} List</h1>
-                {this.props.movies ? <button type="button" id="pick-movie-button" onClick={(e) => this.pickMovie(this.props.movies, this.props.showViewed, this.props.showUnviewed, e)}>Randomly pick a movie</button> : null }
-                
-        </header>
+                {this.props.movies ? <button type="button" id="pick-movie-button" onClick={(e) => this.pickMovie(this.props.movies, this.props.showViewed, this.props.showUnviewed, e)}>Randomly pick a movie</button> : null}
+
+            </header>
         )
     }
 }
